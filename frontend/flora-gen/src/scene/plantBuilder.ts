77eee75479e4cf72fc90f13e305152f30c6a4350
Vinh-Dieu Lam <vinhdieu.lam@gmail.com>
2022-02-17 18:14:10 +0000

    remove spam

--- frontend/flora-gen/src/scene/plantBuilder.ts
+++ frontend/flora-gen/src/scene/plantBuilder.ts
@@ -11,10 +11,8 @@ export function buildPlantMesh({ plant, scene }: { plant: Plant; scene: Scene; }
         nodes.push(new PlantNode(current, scene));
         current = current.successor;
     }
-    console.log(nodes)
-
+    return nodes;
 }
-
 
 function buildTestTubeMesh({ plant, scene }: { plant: Plant; scene: Scene; }): void {
     const path = new Path(new Vector3(0, 0, 0), new Vector3(0, 2, 0), new Vector3(0, 3, -6), new Vector3(3, 5, 5), new Vector3(-4, 8. - 0.5),
