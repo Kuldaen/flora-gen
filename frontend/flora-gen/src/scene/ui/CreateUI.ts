7c21c32608ba67dec8a23626adccf684e072d703
Vinh-Dieu Lam <vinhdieu.lam@gmail.com>
2022-02-24 17:54:50 +0000

    minor cleanup of ui code

--- frontend/flora-gen/src/scene/ui/CreateUI.ts
+++ frontend/flora-gen/src/scene/ui/CreateUI.ts
@@ -28,7 +28,18 @@ const IDEAL_CANVAS_WIDTH = 1000;
 const BUTTON_HEIGHT = 30;
 const BUTTON_WIDTH = 150;
 
-function createButton(options : IButtonOptions): Button {
+export function CreateUI(app: FloraApp) {
+  const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI(
+    "UI",
+    true,
+    app.scene
+  );
+  advancedTexture.idealWidth = IDEAL_CANVAS_WIDTH;
+
+  createCameraControls(advancedTexture, app);
+}
+
+function createButton(options: IButtonOptions): Button {
   const width = options.width_px ? options.width_px : BUTTON_WIDTH;
   const height = options.height_px ? options.height_px : BUTTON_HEIGHT;
   const button = Button.CreateSimpleButton(options.name, options.text);
@@ -44,32 +55,31 @@ function createButton(options : IButtonOptions): Button {
   return button;
 }
 
-export function CreateUI(app: FloraApp) {
-  const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI(
-    "UI",
-    true,
-    app.scene
+function createCameraControls(
+  advancedTexture: AdvancedDynamicTexture,
+  app: FloraApp
+) {
+  advancedTexture.addControl(
+    createButton({
+      name: "up",
+      text: "↑",
+      pos: { x: 10, y: 60 },
+      width_px: 50,
+      onPointerUp: function () {
+        app.setCameraTargetUp();
+      },
+    })
   );
-  advancedTexture.idealWidth = IDEAL_CANVAS_WIDTH;
-  advancedTexture.addControl(createButton({
-    name: "up",
-    text: "↑",
-    pos: { x: 10, y: 60 },
-    width_px: 50,
-    onPointerUp: function () {
-      app.setCameraTargetUp();
-    },
-  }));
 
   advancedTexture.addControl(
-  createButton({
-    name: "down",
-    text: "↓",
-    pos: { x: 10, y: 100 },
-    width_px: 50,
-    onPointerUp: function () {
-      app.setCameraTargetDown();
-    },
-  }));
-
+    createButton({
+      name: "down",
+      text: "↓",
+      pos: { x: 10, y: 100 },
+      width_px: 50,
+      onPointerUp: function () {
+        app.setCameraTargetDown();
+      },
+    })
+  );
 }
